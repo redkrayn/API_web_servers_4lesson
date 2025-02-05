76dd2b8a965fc46a41857b5bcc4aa1687e9337bd
redkrayn <redkrayn@yandex.ru>
2025-02-05 19:14:37 +0800

    Update_telegram_upload_image.py

--- telegram_upload_image.py
+++ telegram_upload_image.py
@@ -3,15 +3,15 @@ import random
 import telegram
 import argparse
 from dotenv import load_dotenv
-from image_data_utils import get_url_image
+from image_data_utils import get_path_file
 
 
 def main():
     load_dotenv()
     chat_id = os.environ["TELEGRAM_CHAT_ID"]
     bot = telegram.Bot(token=os.environ["TELEGRAM_BOT_TOKEN"])
-    random_image = random.choice(get_url_image())
-    parser = argparse.ArgumentParser()
+    random_image = random.choice(get_path_file())
+    parser = argparse.ArgumentParser(description="Загружает указанную картинку в телеграмм, либо рандомную")
     parser.add_argument("name_image", help="название картинки", nargs="?", default=random_image, type=str)
     args = parser.parse_args()
     with open(f'images/{args.name_image}', 'rb') as file:
