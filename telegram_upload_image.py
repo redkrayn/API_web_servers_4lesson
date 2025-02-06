a8c2058acad290e42713f6694092ff99bc0ae62c
redkrayn <redkrayn@yandex.ru>
2025-02-06 16:56:10 +0800

    func rename

--- telegram_upload_image.py
+++ telegram_upload_image.py
@@ -3,14 +3,14 @@ import random
 import telegram
 import argparse
 from dotenv import load_dotenv
-from image_data_utils import get_path_file
+from image_data_utils import get_paths_file
 
 
 def main():
     load_dotenv()
     chat_id = os.environ["TELEGRAM_CHAT_ID"]
     bot = telegram.Bot(token=os.environ["TELEGRAM_BOT_TOKEN"])
-    random_image = random.choice(get_path_file())
+    random_image = random.choice(get_paths_file())
     parser = argparse.ArgumentParser(description="Загружает указанную картинку в телеграмм, либо рандомную")
     parser.add_argument("name_image", help="название картинки", nargs="?", default=random_image, type=str)
     args = parser.parse_args()
